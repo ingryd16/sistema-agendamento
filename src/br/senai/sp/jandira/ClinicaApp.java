15b03f3e2a57da1633d599eadc21980057bdd58c
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2022-09-15 11:04:18 -0300

    alguns objetos criados

--- src/br/senai/sp/jandira/ClinicaApp.java
+++ src/br/senai/sp/jandira/ClinicaApp.java
@@ -1,9 +1,30 @@
 package br.senai.sp.jandira;
 
+import javax.swing.JOptionPane;
+
+import br.senai.sp.jandira.model.Especialidade;
+import br.senai.sp.jandira.model.PlanoDeSaude;
+
 public class ClinicaApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Especialidade especialidade = new Especialidade();
+		especialidade.setNome("x");
+		
+		System.out.println(especialidade.getNome());
+		
+		PlanoDeSaude plano = new PlanoDeSaude();
+		plano.setOperadora("unimed");
+		plano.setTipoDoPlano("bronze");
+
+		JOptionPane.showMessageDialog(null, plano.getDadosDoPlano());
+		
+		PlanoDeSaude planoBradesco = new PlanoDeSaude();
+		planoBradesco.setOperadora("bradesco");
+		planoBradesco.setTipoDoPlano("ouro");
+		JOptionPane.showMessageDialog(null, planoBradesco.getOperadora());
+		JOptionPane.showMessageDialog(null, planoBradesco.getTipoDoPlano());
+		JOptionPane.showMessageDialog(null, planoBradesco.getDadosDoPlano());
 
 	}
 
--- src/br/senai/sp/jandira/model/Especialidade.java
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -1,5 +1,13 @@
 package br.senai.sp.jandira.model;
 
 public class Especialidade {
+	private String nome;
 
+	// Métodos de acesso getters e setters
+	public void setNome(String novoNome) {
+		nome = novoNome;
+	}
+	public String getNome() {
+		return nome;
+	}
 }
